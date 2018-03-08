76cc82463d8d7b07e4a68b0bc3760cd4ecceb9c7
Olufemi Ade-Olusile <f.olusile@gmail.com>
2018-03-08 01:35:06 +0000

    Updating weather-api.js

--- weather-api.js
+++ weather-api.js
@@ -1,6 +1,30 @@
-/**
-* @param context {WebtaskContext}
-*/
-module.exports = function(context, cb) {
-  cb(null, { hello: context.query.name || 'Anonymous' });
-};
+var express    = require('express');
+var Webtask    = require('webtask-tools');
+var bodyParser = require('body-parser');
+var request = require('request');
+var app = express();
+
+app.use(bodyParser.json());
+
+app.post('/weather', function (req, res) {
+  var id =req.param('id');
+  var password = req.param('password');
+  var apiKey = req.webtaskContext.secrets.WEATHER_KEY
+  var city = req.param('city');
+  var url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+
+if(id === 'femi' && password === 'femi'){
+request(url, function (err, response, body) {
+    if(err){
+      res.json({error: 'Error in api call'});
+    } else {
+      var api_body = JSON.parse(body)
+        res.json({weather: `Humidity is ${api_body.main.humidity} and temperature will be ${api_body.main.temp}`});
+      }
+  });
+}else{
+  res.send('Bad username or password');
+}
+});
+
+module.exports = Webtask.fromExpress(app);
